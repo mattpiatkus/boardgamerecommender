804471e3287f91eed35fbb5c290320ad97cddec9
Matt Piatkus <mattpiatkus@gmail.com>
2016-12-29 00:36:42 +0000

    Finally got require.js working

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+require.config({
+  "paths": {
+    "jquery": "https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min",
+    "bootstrap" : "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min"
+  }
+});
+
+
+require(['app/main']);
---
+++ resources/js/app/main.js
@@ -0,0 +1,5 @@
+define(['jquery', 'bootstrap'], function () {
+    $("#sign").on('click', function () {
+        alert("signclick")
+    })
+})
